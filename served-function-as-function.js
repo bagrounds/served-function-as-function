f8b88a9d9ddb992248b5ae96ad2858b41859ecb0
bgrounds <bgrounds@ea.com>
2016-06-22 19:48:28 -0700

    [LINT]

--- served-function-as-function.js
+++ served-function-as-function.js
@@ -2,23 +2,21 @@
  *
  * @module served-function-as-function
  */
-(function(){
-  "use strict";
+(function () {
+  'use strict'
 
-  /*****************************************************************************
+  /**
    * imports
    */
-  var queryString = require('query-string');
-  var request = require('request');
-  var _ = require('lodash');
-  var lomath = require('lomath');
+  var queryString = require('query-string')
+  var request = require('request')
+  var _ = require('lodash')
+  var lomath = require('lomath')
 
-
-
-  /*****************************************************************************
+  /**
    * exports
    */
-  module.exports = servedFunctionAsFunction;
+  module.exports = servedFunctionAsFunction
 
   /**
    * Use a REST API as if it were a function
@@ -27,113 +25,98 @@
    * @alias served-function-as-function
    *
    * @param {String} url base address for the REST API
-   * @param {Object} options
-   * @param {*} [options.data]
-   * @param {Function} callback
+   * @param {Object} options all function parameters
+   * @param {*} [options.data] a large function parameter
+   * @param {Function} callback handle results
    */
-  function servedFunctionAsFunction(url, options, callback){
-
-    if( options.data ){
-      console.log('POST');
-      httpPost(url,options,callback);
+  function servedFunctionAsFunction (url, options, callback) {
+    if (options.data) {
+      console.log('POST')
+      httpPost(url, options, callback)
     } else {
-      console.log('GET');
-      httpGet(url,options,callback);
+      console.log('GET')
+      httpGet(url, options, callback)
     }
-
   }
 
-  function httpGet(url, options, callback){
+  function httpGet (url, options, callback) {
+    var query = '?' + queryString.stringify(options)
 
-    var query = '?' + queryString.stringify(options);
+    url = url + query
 
-    url = url + query;
-
-    request(url, function(error, response, body) {
-
-      if( isProblem(error,response) ){
-        error = handleProblem(error, response, body);
-        callback(error, body);
-        return;
+    request(url, function (error, response, body) {
+      if (isProblem(error, response)) {
+        error = handleProblem(error, response, body)
+        callback(error, body)
+        return
       }
 
-      if( body ){
-        body = JSON.parse(body);
+      if (body) {
+        body = JSON.parse(body)
       }
 
-      callback(error, body);
-    });
+      callback(error, body)
+    })
   }
 
-  function isProblem(error, response){
-    return  error || (response && response.statusCode != 200);
+  function isProblem (error, response) {
+    return error || (response && response.statusCode !== 200)
   }
 
-  function handleProblem(error, response, body){
+  function handleProblem (error, response, body) {
+    var e = {}
 
-    var e = {};
-
-    if( error ){
-      e.error = error;
+    if (error) {
+      e.error = error
     }
 
-    if( (response && response.statusCode != 200)){
-      e.statusCode = response.statusCode;
+    if ((response && response.statusCode !== 200)) {
+      e.statusCode = response.statusCode
     }
 
-    return e;
+    return e
   }
 
   /**
    *
    * @param {String} url base address for the REST API
-   * @param {Object} options
-   * @param {*} options.data
-   * @param {Function} callback
+   * @param {Object} options function parameters
+   * @param {*} options.data a large input parameter
+   * @param {Function} callback handle results
    */
-  function httpPost(url, options, callback){
+  function httpPost (url, options, callback) {
+    var data = options.data
 
-    var data = options.data;
+    options = _.omit(options, 'data')
 
-    options = _.omit(options, 'data');
+    var query = '?' + queryString.stringify(options)
 
-    var query = '?' + queryString.stringify(options);
-
-    url = url + query;
-
-    console.log(data);
-
-    //var formData = lomath.flattenJSON(data);
+    url = url + query
 
     var requestOptions = {
       url: url,
       json: data
-    };
+    }
 
-    console.log('request from: ' + url);
+    console.log('request from: ' + url)
     request.post(requestOptions, function (error, response, body) {
+      error && console.error(error)
 
-      error && console.error(error);
-
-      if( typeof body == 'string' ){
-        console.log('its a string!');
-        body = JSON.parse(body);
+      if (typeof body === 'string') {
+        body = JSON.parse(body)
       }
 
-      if( isProblem(error,response) ){
-        console.error('A PROBLEM!!!');
-        error = handleProblem(error, response, body);
-        callback(error, body);
-        return;
+      if (isProblem(error, response)) {
+        error = handleProblem(error, response, body)
+        callback(error, body)
+        return
       }
 
-      if( body ){
-        console.log('flat body: '  + JSON.stringify(body));
-        body = lomath.unflattenJSON(body);
-        console.log('unflattened body: '  + JSON.stringify(body));
+      if (body) {
+        body = lomath.unflattenJSON(body)
       }
 
-      callback(error, body);
-    });
+      callback(error, body)
+    })
   }
-})();
+})()
